b882fc8c0d9ab1772eb74e22ae51d55a23d50be4
nickkuk <nickkuklin@gmail.com>
2020-08-12 21:46:20 +0500

    Use unbounded channels and tokio::try_join.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use reqwest::{
   header::{HeaderMap, HeaderValue, CONTENT_TYPE},
   Body, Response,
 };
-use tokio::{fs::File, stream::StreamExt};
+use tokio::{
+  fs::File,
+  stream::StreamExt,
+  sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+};
 use tokio_util::codec::{BytesCodec, FramedRead};
 
 const JSON_SIZE: usize = 500_000;
@@ -34,18 +38,16 @@ fn create_file(path: &str, size: usize) -> Result<(), std::io::Error> {
   Ok(())
 }
 
-async fn upload_file(path: &str, url: &str) -> Result<Response, Box<dyn std::error::Error>> {
-  let file = File::open(path).await?;
-  let metadata = file.metadata().await?;
-  let file_size = metadata.len();
-  let pb = new_progress_bar(file_size);
-  let pb_clone = pb.clone();
-
+async fn upload_file(
+  file: File,
+  url: &str,
+  sender: UnboundedSender<u64>,
+) -> Result<Response, Box<dyn std::error::Error>> {
   let mut stream = FramedRead::new(file, BytesCodec::new());
   let stream_progressed = async_stream::stream! {
     let mut last_inc = 0;
     while let Some(bytes) = stream.next().await {
-      pb_clone.inc(last_inc);
+      sender.send(last_inc).unwrap();
       if let Ok(bytes) = &bytes {
         last_inc = bytes.len() as u64;
       }
@@ -57,14 +59,32 @@ async fn upload_file(path: &str, url: &str) -> Result<Response, Box<dyn std::err
   let mut headers = HeaderMap::new();
   headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
   let response = reqwest::Client::new().post(url).headers(headers).body(body).send().await?;
-  pb.finish_with_message("uploaded");
   Ok(response)
+}
+
+async fn progress_waiter(
+  file_size: u64,
+  mut receiver: UnboundedReceiver<u64>,
+) -> Result<(), Box<dyn std::error::Error>> {
+  let pb = new_progress_bar(file_size);
+  while let Some(p) = receiver.recv().await {
+    pb.inc(p);
+  }
+  pb.finish_with_message("uploaded");
+  Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   create_file(FILE_PATH, JSON_SIZE)?;
-  let response = upload_file(FILE_PATH, URL).await?;
+
+  let file = File::open(FILE_PATH).await?;
+  let metadata = file.metadata().await?;
+  let file_size = metadata.len();
+
+  let (sender, receiver) = unbounded_channel();
+  let response = tokio::try_join!(progress_waiter(file_size, receiver), upload_file(file, URL, sender))?.1;
   println!("{:?}", response);
+
   Ok(())
 }
